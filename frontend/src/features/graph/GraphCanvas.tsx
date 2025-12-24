83d0ef58eff81111fbdf5b2c2dfbef962c063291
Anton Zamyatin <zamyatin.anton@gmail.com>
2025-12-24 02:02:47 -0500

    reset graph layout on sidebar collapse fix, minor UI changes

--- frontend/src/features/graph/GraphCanvas.tsx
+++ frontend/src/features/graph/GraphCanvas.tsx
@@ -1,24 +1,33 @@
 import { SigmaContainer } from "@react-sigma/core";
 import { GraphLoader } from "./GraphLoader";
 import { HoverCoreNode } from "./HoverCoreNode";
+import { useMemo } from "react";
+//import { SigmaAutoResize } from "./SigmaAutoResize";
 
-export function GraphCanvas() {
-  return (
-    <div style={{ width: "100%", height: "100%" }}>
-      <SigmaContainer
-        style={{ width: "100%", height: "100%" }}
-        settings={{
 
-          defaultNodeColor: "#9aa0a6",
-          defaultEdgeColor: "#9aa0a6",
-          labelRenderedSizeThreshold: 10,
-          enableEdgeEvents: true,
-          defaultDrawNodeHover: () => {},
-        }}
-      >
-        <GraphLoader />
-        <HoverCoreNode />
-      </SigmaContainer>
-    </div>
-  );
+
+export function GraphCanvas() { 
+    
+    const sigmaSettings = useMemo(
+        () => ({
+            defaultNodeColor: "#9aa0a6",
+            defaultEdgeColor: "#9aa0a6",
+            labelRenderedSizeThreshold: 10,
+            enableEdgeEvents: true,
+            defaultDrawNodeHover: () => {},
+        }),
+        []
+      );
+
+    return (
+        <div style={{ width: "100%", height: "100%" }}>
+        <SigmaContainer
+            style={{ width: "100%", height: "100%" }}
+            settings={sigmaSettings}
+        >
+            <GraphLoader />
+            <HoverCoreNode />
+        </SigmaContainer>
+        </div>
+    );
 }
